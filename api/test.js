da30187becfefc5f45be4b1e37a9ab3b950706ff
Eugeniu Cazmal <eugeniucazmal@gmail.com>
2025-11-07 11:19:22 +0200

    Reduce serverless functions: remove unused index/init/test

--- api/test.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({ 
-    message: "API funcționează perfect!",
-    timestamp: new Date().toISOString(),
-    method: req.method
-  });
-}
-
